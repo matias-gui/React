879da782c7410460d8c205febbbc69da441e1044
Guilherme Matias <guimatias890@gmail.com>
2025-03-18 21:47:46 -0300

    ajustando o cadastro

--- react-cadastro/src/cadastro/index.jsx
+++ react-cadastro/src/cadastro/index.jsx
@@ -2,21 +2,30 @@ import { Link } from "react-router"
 
 function Cadastro() {
   return (
-    <body className="w-dvw h-dvh flex flex-wrap  justify-center bg-[url(/src/img/pexels-dennisleinarts-1437953.jpg)] bg-center bg-no-repeat bg-cover bg-fixed"   >
+    <body className="w-dvw h-dvh flex flex-wrap  justify-center bg-[url(src/img/pexels-ivan-cujic-20495-109479.jpg)] bg-center bg-no-repeat bg-cover bg-fixed"   >
       
-        <main className=" flex flex-col justify-center max-w-120 h-65 border border-transparent rounded-xl mt-15 p-2 shadow-[2px_2px_10px_black]  backdrop-blur-[15px]"  >
-          <h2 className="text-center text-xl mb-4 font-mono font-bold animate-bounce">Cadastro</h2>
+        <main className=" flex flex-col justify-center max-w-120 h-65 border border-transparent rounded-xl mt-15 p-2 shadow-[2px_2px_10px_black]  backdrop-blur-[20px]"  >
+          <h2 className="text-center text-xl mb-4 font-mono font-bold animate-bounce ">Cadastro</h2>
           <form>
-            <input placeholder="Nome" type="text" className="border w-full mb-2 indent-3 rounded-md p-1 placeholder-black opacity-70" />
+            <input placeholder="Nome" type="text" className="border w-full mb-2 indent-3 rounded-md p-1 placeholder-black opacity-70 " />
             <input placeholder="E-mail" type="email" className=" border  w-full mb-2 indent-3 rounded-md p-1 placeholder-black  opacity-70" />
             <input placeholder="Senha" type="password" className=" border  w-full mb-2 indent-3 rounded-md p-1 placeholder-black  opacity-70 " />
-            <button className=" w-full rounded-md border-none mb-1 mt-2 bg-cyan-950 text-white p-1  hover:opacity-85 cursor-pointer delay-200  hover:scale-101 duration-100 ease-in-out font-bold " >Cadastrar</button>
+            <button className=" w-full rounded-md border-none mb-1 mt-2 bg-cyan-950 text-white p-1  hover:opacity-80   cursor-pointer delay-200  hover:scale-101 duration-100 ease-in-out font-bold " onClick={cadastrar()} >Cadastrar</button>
           </form>
-          <Link to="/login" className="text-center mb-1 font-bold hover:underline">Já tem uma conta?
+          <Link to="/login" className="text-center mb-1 font-mono font-bold hover:underline">Já tem uma conta?
             Faça login</Link>
         </main>
     </body>
   )
+}
+let nome = document.getElementsByTagName("text")
+let email = document.getElementsByClassName("email")
+let senha = document.getElementsByClassName("password")
+
+function cadastrar(){
+  if(nome.length === 0 || nome === ""){
+    alert("ERRO")
+  }
 }
 
 export default Cadastro
