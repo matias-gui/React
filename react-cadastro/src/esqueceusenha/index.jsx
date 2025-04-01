0589cdba261c96082b1c44bb266c04ec4c2ad8ec
Guilherme Matias <guimatias890@gmail.com>
2025-04-01 20:08:40 -0300

    criação da pagina para recuperar senha

--- react-cadastro/src/esqueceusenha/index.jsx
+++ react-cadastro/src/esqueceusenha/index.jsx
@@ -2,15 +2,12 @@
 
 function Esqueceusenha(){
     return(
-        <body className="flex justify-center w-full h-full">
-            <div className="">
-                <form className="flex flex-col w-[90%] border rounded-2xl  " >
-                    <input type="text" name="nome" id="inome" placeholder="Nome" className="border
-                    rounded-[5px] indent-1 "/>
-                    <input type="email" name="email" id="iemail"/>
-                </form>
-            </div>
-        </body>
+        <div className="border w-120 h-50 m-auto mt-1 flex  ">
+            <form className="w-[100%] flex-row justify-items-center">
+                <input type="email" name="email" id="iemail" placeholder="Email" className="border rounded-[7px] flex flex-row justify-center mt-3 w-[80%] indent-2" />
+                <input type="text" name="num" id="inum" placeholder=" Código" className="border rounded-[7px] flex flex-row  mt-2  w-[80%] indent-2" />
+            </form>
+        </div>
     )
 }
 
