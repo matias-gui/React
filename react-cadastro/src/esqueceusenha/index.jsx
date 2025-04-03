793ac97336f74601046b45270a31a45106a9887c
Guilherme Matias <guimatias890@gmail.com>
2025-04-02 22:14:21 -0300

    Update index.jsx

--- react-cadastro/src/esqueceusenha/index.jsx
+++ react-cadastro/src/esqueceusenha/index.jsx
@@ -1,13 +1,17 @@
 
 
-function Esqueceusenha(){
-    return(
-        <div className="border w-120 h-50 m-auto mt-1 flex  ">
+function Esqueceusenha() {
+    return (
+        <div className="border w-120 h-50 m-auto mt-1 flex flex-col rounded-[7px]  ">
             <form className="w-[100%] flex-row justify-items-center">
                 <input type="text" name="text" id="itext" placeholder="Nome" className="border rounded-[7px] flex flex-row justify-center mt-3 w-[80%] indent-2" />
                 <input type="email" name="email" id="iemail" placeholder="Email" className="border rounded-[7px] flex flex-row  mt-2  w-[80%] indent-2" />
-                <div className="border flex-row justify-items-end h-7 mt-2 rounded-[7px]"><input type="text" className="border rounded-[7px] flex flex-row  mt-2  w-[80%] indent-2 translate-[-7px]" /></div>
+                <div className="border w-[80%] bg-black text-white mt-2 rounded-[7px]  flex ">
+                     <p className=" indent-2 pr-2 ">HGfg</p>
+                    <input type="text" placeholder="Digite o código" className="flex border rounded-[7px] bg-white text-black   w-[90%] indent-2 " />
+                </div>
             </form>
+            <button className="border w-[80%] m-auto rounded-[7px] bg-black text-white hover:opacity-70 ">Recuperar senha</button>
         </div>
     )
 }
